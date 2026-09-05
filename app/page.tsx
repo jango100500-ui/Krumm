077128a6f047c166db6c15c0f86cb1e573ec2bbb
jango100500-ui <jango100500@gmail.com>
2026-09-05 18:03:58 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -125,17 +125,6 @@ export default function Home() {
   return (
     <main className="fixed inset-0 w-full h-[100dvh] bg-[#0a0a0a] overflow-hidden">
       
-      <div 
-        className="absolute inset-x-0 bottom-[-80px] bg-[#1A1A1C] pointer-events-none z-0"
-        style={{ top: 'calc(50% - 165px)' }}
-      >
-        <img
-          src="/clouds.png"
-          alt="Clouds"
-          className="absolute bottom-full left-0 w-full h-auto object-cover pointer-events-none block"
-        />
-      </div>
-
       {isBlurred && (
         <div
           onClick={handleDismiss}
